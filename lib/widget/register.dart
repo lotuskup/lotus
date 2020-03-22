f7e0fb15342b3a97a0605850f67752c3a8bc66ea
lotuskup <lotuskup@gmail.com>
2020-03-22 14:28:58 +0700

    Register success

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -1,6 +1,9 @@
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:math';
 import 'dart:ui';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lotus/utility/narmal_dialog.dart';
@@ -116,15 +119,55 @@ class _RegisterState extends State<Register> {
         if (file == null) {
           narmaldialog(context, 'Non Choose Avatar',
               'Please Click Image For Open Camara');
-        }else if (name==null || name.isEmpty||user==null || user.isEmpty|| password==null || password.isEmpty) {
-          narmaldialog(context, 'Have Space',
-              'Please Fill Every Blank');
+        } else if (name == null ||
+            name.isEmpty ||
+            user == null ||
+            user.isEmpty ||
+            password == null ||
+            password.isEmpty) {
+          narmaldialog(context, 'Have Space', 'Please Fill Every Blank');
         } else {
+          uploadImageTosever();
+          
         }
       },
     );
   }
 
+  // upload to server
+  Future<void> uploadImageTosever() async {
+    try {
+      String url = 'https://www.androidthai.in.th/rice/saveFileLotus.php';
+      Map<String, dynamic> map = Map();
+      Random random = Random();
+      int i = random.nextInt(100000);
+      map['file'] = UploadFileInfo(file, 'user$i.jpg');
+      FormData formData = FormData.from(map);
+      var response = await Dio().post(url, data: formData);
+      print('response = ${response.toString()}');
+      urlPath = 'https://www.androidthai.in.th/rice/lotus/user$i.jpg';
+      insertDataToMySQL();
+    } catch (e) {
+      print('error --> ${e.toString()}');
+    }
+  }
+
+// end upload
+  Future<void> insertDataToMySQL() async {
+try {
+  String url = 'https://www.androidthai.in.th/rice/addUserLotus.php?isAdd=true&Name=$name&User=$user&Password=$password&Avatar=$urlPath';
+  var response = await Dio().get(url);
+  if (response.toString() == 'true' ) {
+    Navigator.of(context).pop();
+  } else {
+    narmaldialog(context, 'Can not Register', 'Please Try Agains');
+  }
+  
+} catch (e) {
+}
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
