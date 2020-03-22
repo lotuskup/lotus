c2b017d86fad3c8d179bc7ddfd11e90b63811236
lotuskup <lotuskup@gmail.com>
2020-03-22 16:01:42 +0700

    pass data success

--- lib/widget/my_service.dart
+++ lib/widget/my_service.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+
 class Myservice extends StatefulWidget {
-
-final String name;
-Myservice({Key key,this.name}):super(key:key);
-
+  final String name;
+  Myservice({Key key, this.name}) : super(key: key);
 
   @override
   _MyserviceState createState() => _MyserviceState();
 }
 
 class _MyserviceState extends State<Myservice> {
+  // field
+  String nameLogin;
+
+  // method
+
+  @override
+  void initState() {
+    super.initState();
+    nameLogin = widget.name;
+  }
+
+  Widget showNameLogin() {
+    return Column(
+      children: <Widget>[
+        Text('Login By'),
+        Text(nameLogin),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My service'),),
+      appBar: AppBar(
+        actions: <Widget>[showNameLogin()],
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('My service'),
+      ),
     );
   }
 }
